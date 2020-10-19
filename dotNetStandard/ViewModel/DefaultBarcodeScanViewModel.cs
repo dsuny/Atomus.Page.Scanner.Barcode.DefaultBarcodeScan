f87f70f38ae3feff3dbb1d13fbc6bf7f199d1082
권대선 <dsun@hanmail.net>
2020-10-19 18:43:25 +0900

    신규

---
+++ dotNetStandard/ViewModel/DefaultBarcodeScanViewModel.cs
@@ -0,0 +1,149 @@
+﻿using Atomus.Scanner.Barcode;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace Atomus.Page.Scanner.Barcode.ViewModel
+{
+    public class DefaultBarcodeScanViewModel : MVVM.ViewModel
+    {
+        #region Declare
+        private ICore core;
+
+        List<BarcodeResult> list;
+        string scanResult;
+        private bool isEnabledControl;
+        #endregion
+
+        #region Property
+        public ICore Core
+        {
+            get
+            {
+                return this.core;
+            }
+            set
+            {
+                this.core = value;
+            }
+        }
+
+        public List<BarcodeResult> BarcodeResult
+        {
+            get
+            {
+                return this.list;
+            }
+            set
+            {
+                this.list = value;
+            }
+        }
+        public string ScanResult
+        {
+            get
+            {
+                this.scanResult = string.Empty;
+                foreach (BarcodeResult barcodeResult in this.list)
+                    this.scanResult += $"Type : {barcodeResult.BarcodeValueFormat}, Value : {barcodeResult.DisplayValue}{Environment.NewLine}";
+
+                return this.scanResult;
+            }
+            set
+            {
+                this.scanResult = value;
+            }
+        }
+        public bool IsEnabledControl
+        {
+            get
+            {
+                return this.isEnabledControl;
+            }
+            set
+            {
+                if (this.isEnabledControl != value)
+                {
+                    this.isEnabledControl = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+
+        public ICommand BackCommand { get; set; }
+        public ICommand FlashlightCommand { get; set; }
+        public ICommand DetectedCommand { get; set; }
+        #endregion
+
+        #region INIT
+        public DefaultBarcodeScanViewModel()
+        {
+        }
+        public DefaultBarcodeScanViewModel(ICore core) : this()
+        {
+            this.Core = core;
+
+            this.list = new List<BarcodeResult>();
+            this.isEnabledControl = true;
+
+            this.BackCommand = new Command(() => this.BackProcess()
+                                            , () => { return this.IsEnabledControl; });
+
+            this.FlashlightCommand = new Command(async () => await this.FlashlightProcess()
+                                            , () => { return this.IsEnabledControl; });
+
+
+            this.DetectedCommand = new Command(() => this.DetectedProcess()
+                                            , () => { return this.IsEnabledControl; });
+        }
+        #endregion
+
+        #region IO
+        internal void BackProcess()
+        {
+            this.IsEnabledControl = false;
+            (this.BackCommand as Command).ChangeCanExecute();
+
+            (this.core as IAction).ControlAction(this, new AtomusPageArgs("Back", null));
+
+            this.IsEnabledControl = true;
+            (this.BackCommand as Command).ChangeCanExecute();
+        }
+        private async Task FlashlightProcess()
+        {
+            try
+            {
+                this.IsEnabledControl = false;
+                (this.FlashlightCommand as Command).ChangeCanExecute();
+
+                DependencyService.Get<IBarCodeCamera>()?.ToggleFlashlight();
+            }
+            catch (Exception ex)
+            {
+                await Application.Current.MainPage.DisplayAlert("Warning", ex.Message, "OK");
+            }
+            finally
+            {
+                this.IsEnabledControl = true;
+                (this.FlashlightCommand as Command).ChangeCanExecute();
+            }
+        }
+        internal void DetectedProcess()
+        {
+            this.IsEnabledControl = false;
+            (this.DetectedCommand as Command).ChangeCanExecute();
+
+            (this.core as IAction).ControlAction(this, new AtomusPageArgs("Barcode.Detected", this.list));
+            //await ((NavigationPage)Application.Current.MainPage).PopAsync();
+
+            this.IsEnabledControl = true;
+            (this.DetectedCommand as Command).ChangeCanExecute();
+        }
+        #endregion
+
+        #region ETC
+        #endregion
+    }
+}
---
+++ dotNetStandard/Views/DefaultBarcodeScan.xaml.cs
@@ -0,0 +1,95 @@
+﻿using Atomus.Page.Scanner.Barcode.ViewModel;
+using Atomus.Scanner.Barcode;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Atomus.Page.Scanner.Barcode
+{
+
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class DefaultBarcodeScan : ContentPage, IAction
+    {
+        //public event ReturnBarcodeScannedEventHandler ReturnBarcodeScannedEvent;
+        private AtomusPageEventHandler beforeActionEventHandler;
+        private AtomusPageEventHandler afterActionEventHandler;
+
+        #region INIT
+        public DefaultBarcodeScan()
+        {
+            this.BindingContext = new DefaultBarcodeScanViewModel(this);
+
+            InitializeComponent();
+
+            NavigationPage.SetHasNavigationBar(this, false);
+
+            this.BackgroundColor = ((string)Config.Client.GetAttribute("BackgroundColor")).ToColor();
+        }
+        #endregion
+
+        #region IO
+        object IAction.ControlAction(ICore sender, AtomusPageArgs e)
+        {
+            try
+            {
+                this.beforeActionEventHandler?.Invoke(this, e);
+
+
+                //switch (e.Action)
+                //{
+                //    case "Back":
+                //        Navigation.PopModalAsync();
+                //        break;
+
+                //    case "Barcode.Detected":
+                //        this.afterActionEventHandler?.Invoke(this, e);
+                //        break;
+                //}
+
+                return true;
+            }
+            finally
+            {
+                Device.BeginInvokeOnMainThread(() =>
+                {
+                    this.afterActionEventHandler?.Invoke(this, e);
+                });
+
+                //if (e.Action != "Barcode.Detected")
+            }
+        }
+        #endregion
+
+        #region Event
+        event AtomusPageEventHandler IAction.BeforeActionEventHandler
+        {
+            add
+            {
+                this.beforeActionEventHandler += value;
+            }
+            remove
+            {
+                this.beforeActionEventHandler -= value;
+            }
+        }
+        event AtomusPageEventHandler IAction.AfterActionEventHandler
+        {
+            add
+            {
+                this.afterActionEventHandler += value;
+            }
+            remove
+            {
+                this.afterActionEventHandler -= value;
+            }
+        }
+
+        private void CameraView_OnDetected(object sender, DetectedEventArg e)
+        {
+            (this.BindingContext as DefaultBarcodeScanViewModel).BarcodeResult = e.BarcodeResults;
+            (this.BindingContext as DefaultBarcodeScanViewModel).DetectedCommand.Execute(e);
+        }
+        #endregion
+    }
+}
